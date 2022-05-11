c1751544665102a9a292cf4a9a1393b4069df729
zeromountain <sonyeongsan@gmail.com>
2022-05-11 18:44:33 +0900

    feat: implement add todo function

--- client/components/HomePage/_fragments/_fragments/TodoInput.tsx
+++ client/components/HomePage/_fragments/_fragments/TodoInput.tsx
@@ -1,12 +1,28 @@
 import { AddIcon } from '@chakra-ui/icons';
 import { Box, Flex, Input } from '@chakra-ui/react';
-import React from 'react';
+import React, { useRef } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+import { useAppDispatch } from '../../../../features/store';
+import { addTodo } from '../../../../features/Todo/todoSlice';
 
 const TodoInput = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const dispatch = useAppDispatch();
+
+  const handleAddTodo = () => {
+    dispatch(
+      addTodo({
+        id: uuidv4(),
+        content: inputRef.current?.value,
+      })
+    );
+  };
   return (
     <>
       <Flex w='700px' h='50px' lineHeight='50px' borderRadius='5px'>
         <Input
+          ref={inputRef}
           placeholder='할 일을 입력해 주세요.'
           w='calc(100% - 3rem)'
           borderStyle='none'
@@ -22,7 +38,7 @@ const TodoInput = () => {
           w='3rem'
           borderRadius='0 5px 5px 0'
           cursor='pointer'>
-          <AddIcon w='3rem' h='2rem' color='white' />
+          <AddIcon w='3rem' h='2rem' color='white' onClick={handleAddTodo} />
         </Box>
       </Flex>
     </>
