dd2cee5f3d49fe9774b8c93cd412d689c0e7c6f1
zeromountain <sonyeongsan@gmail.com>
2022-05-11 18:44:07 +0900

    feat: get todo state from store

--- client/components/HomePage/_fragments/HomePageContent.tsx
+++ client/components/HomePage/_fragments/HomePageContent.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
+import { useAppSelector } from '../../../features/store';
+
 import TodoHeader from './_fragments/TodoHeader';
 import TodoInput from './_fragments/TodoInput';
 import TodoList from './_fragments/TodoList';
 
-const todos = [
-  { id: '12442351sadfasf', content: '투두리스트만들기' },
-  { id: 'sadgxcjlagjqtjqlkj', content: '투두리스트 차크라 적용' },
-  { id: 'czxvmnagjqiowetowqjgksda', content: '투두리스트 리액트 쿼리 적용' },
-  { id: 'cx,mvnzklgputqtasdjflkjsa', content: '투두리스트 타입스크립트 적용' },
-];
-
 const HomePageContent = () => {
+  const { todos } = useAppSelector(({ TODO }) => TODO);
+
   return (
     <>
       <TodoHeader />
